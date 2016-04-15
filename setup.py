b5bbd14773b381265fae9bfd33c05b6aa5c4ef6b
twsswt <timothy.storer@glasgow.ac.uk>
2016-04-15 13:13:14 +0100

    Added test suite to setup script.

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@ setup(
     name='Fuzzi_Moss',
     version='0.1',
     packages=['fuzzi_moss'],
-    package_dir={'': 'src/main/python' },
+    package_dir={'': './' },
     url='https://github.com/twsswt/fuzzi-moss',
     license='',
     author='Tom Wallis, Tim Storer',
     author_email='',
     description='Fuzzing for Modelling Variance in Socio-technical Systems',
-    test_suite='src/tests/python/unittests'
+    test_suite='tests.unittests',
+    tests_require=['mock']
 )
