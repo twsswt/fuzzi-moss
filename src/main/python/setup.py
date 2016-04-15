12b5fe7baa0ce135b4ada5d3608a88b3c75e9422
twsswt <timothy.storer@glasgow.ac.uk>
2016-04-15 12:20:06 +0100

    Added setup.py for automated deployment.

---
+++ src/main/python/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Fuzzi_Moss',
+    version='0.1',
+    packages=['fuzzi_moss'],
+    package_dir={'': 'src/main/python'},
+    url='https://github.com/twsswt/fuzzi-moss',
+    license='',
+    author='Tom Wallis, Tim Storer',
+    author_email='',
+    description='Fuzzing for Modelling Variance in Socio-technical Systems'
+)
