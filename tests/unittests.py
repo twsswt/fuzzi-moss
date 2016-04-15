fa10a4d78be75b6a3e1db99f80e473b3b3193192
twsswt <timothy.storer@glasgow.ac.uk>
2016-04-15 14:17:19 +0100

    Modified unit tests to generate XML output.

--- tests/unittests.py
+++ tests/unittests.py
@@ -174,4 +174,5 @@ class FuzziMossTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
