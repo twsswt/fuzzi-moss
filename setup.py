ee41129de806c7350c279c83c56bcb592f82b053
twsswt <timothy.storer@glasgow.ac.uk>
2016-04-18 12:56:18 +0100

    Tidied up requirements and setup.

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
     name='Fuzzi_Moss',
     version='0.1',
     packages=['fuzzi_moss'],
-    package_dir={'': './' },
+    package_dir={'': '.'},
     url='https://github.com/twsswt/fuzzi-moss',
     license='',
     author='Tom Wallis, Tim Storer',
-    author_email='',
+    author_email='twallisgm@gmail.com',
     description='Fuzzing for Modelling Variance in Socio-technical Systems',
     setup_requires=['nose'],
     test_suite='nose.collector',
-    tests_require=['mock','nose']
+    tests_require=['mock', 'nose']
 )
