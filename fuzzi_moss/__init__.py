df8e19cdb0d7cf52f08feb666c20dd9e719e6f47
twsswt <timothy.storer@glasgow.ac.uk>
2016-06-07 16:38:34 +0100

    Corrected missing import to fuzzi_moss public API.  Renamed test files to follow Python conventions.

--- fuzzi_moss/__init__.py
+++ fuzzi_moss/__init__.py
@@ -3,5 +3,6 @@ Front end API for the fuzzi_moss library.
 """
 
 from fuzzi_moss.core_fuzzers import *
+from fuzzi_moss.sociotech_fuzzers import *
 from fuzzi_moss.fuzz_decorator import fuzz
 from fuzzi_moss.fuzz_weaver import fuzz_clazz, fuzz_module
